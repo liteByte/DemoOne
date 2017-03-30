a97a66e050e2b3894e3c3d1f83b3f0e0445e5ffa
Gonza <gonzalo.medinarosa@litebyte.us>
2017-03-30 00:04:45 -0300

    Add alert component

---
+++ src/components/alert/Alert.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Dialog from "material-ui/Dialog";
+
+export default class GenericAlert extends React.Component {
+  render() {
+    return (
+      <Dialog
+        title={this.props.title}
+        actions={this.props.children}
+        modal={false}
+        open={this.props.open}
+        onRequestClose={this.props.exit}
+        autoScrollBodyContent={true}
+      >
+        {this.props.text}
+      </Dialog>
+    )
+  }
+}
+
+GenericAlert.PropTypes = {
+  title: React.PropTypes.string.isRequired,
+  text: React.PropTypes.string.isRequired,
+  open: React.PropTypes.bool.isRequired,
+  exit: React.PropTypes.func.isRequired,
+};
